462005744591ffbef8deaf8f7fad90d1f9c4da81
TakeOto <thismailboxforspam@gmail.com>
2023-05-03 23:31:53 +0300

    the warning message

--- src/Constract/ErrorMessageInterface.php
+++ src/Constract/ErrorMessageInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Takeoto\Message\Contract;
 
 interface ErrorMessageInterface extends MessageInterface
--- src/WarningMessage.php
+++ src/WarningMessage.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Takeoto\Message;
 
-class WarningMessage extends Message
+use Takeoto\Message\Contract\WarningMessageInterface;
+
+class WarningMessage extends Message implements WarningMessageInterface
 {
 }
